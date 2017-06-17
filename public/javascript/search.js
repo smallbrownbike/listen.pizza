47f186d161978708b4c28e1d9c138654c0df98b0
smallbrownbike <snscrn@gmail.com>
2017-06-16 19:35:05 -0700

    added expandable albums list div

--- public/javascript/search.js
+++ public/javascript/search.js
@@ -1,4 +1,6 @@
 const container = document.getElementById(''),
+searchAlbumList = document.getElementById('searchAlbumList'),
+expand = document.getElementById('expand'),
 title = document.getElementsByTagName('h7');
 
 if (matchMedia) {
@@ -10,15 +12,16 @@ if (matchMedia) {
 // media query change
 function WidthChange(mq) {
   if (mq.matches) {
-    albumList.setAttribute('class', 'ui four cards')
+    searchAlbumList.setAttribute('class', 'ui four cards')
   } else {
-    albumList.setAttribute('class', 'ui two cards')
+    searchAlbumList.setAttribute('class', 'ui two cards')
   }
 }
 
+
 function searchListener(){
-	var arr = JSON.parse(this.responseText);
-	showContent(arr);
+	data = JSON.parse(this.responseText);
+	showContent(data);
 	
 }
 
@@ -36,7 +39,7 @@ xhr.send();
 function showContent(data){
 	var html = ''
 	
-	for(var i=0;i<48;i++){
+	for(var i=0;i<4;i++){
 		var image;
 		var artist = data.topalbums.album[i].artist.name.replace(/[!'()*]/g, escape);
 		var album = data.topalbums.album[i].name.replace(/[!'()*]/g, escape);
@@ -45,14 +48,37 @@ function showContent(data){
 		} else {
 			image = 'https://s-media-cache-ak0.pinimg.com/originals/b8/9d/17/b89d17a8d96248e8ce344de075372c24.jpg';
 		}
-		
-		html += "<div id='card' class='card'><a class='image' id='imageContainer' href='/album/" + encodeURIComponent(artist) + '+' + encodeURIComponent(album) + "'><img id='image' class='img' src='" + image + "'></a><div id='cardInfo' class='ui center aligned container'><h4 id='cardArtist'>" + decodeURI(artist) + "</h4><h7>" + decodeURI(album) + "</h7></div></div>"
+		html += "<div id='card' class='searchAlbumCard ui card'><a class='image' id='imageContainer' href='/album/" + encodeURIComponent(artist) + '+' + encodeURIComponent(album) + "'><img id='image' class='img' src='" + image + "'></a><div id='cardInfo' class='ui center aligned container'><h4 id='cardArtist'>" + decodeURI(artist) + "</h4><h7>" + decodeURI(album) + "</h7></div></div>"
 	}
 	
-	albumList.innerHTML = html;
+	searchAlbumList.innerHTML = html;
 	for(var i=0; i < title.length; i++){
 	if(title.item(i).textContent.length > 28){
 		title.item(i).textContent = title.item(i).textContent.slice(0, 25) + '...'
 	}
 }
+	for(var i=4;i<48;i++){
+		var image;
+		var artist = data.topalbums.album[i].artist.name.replace(/[!'()*]/g, escape);
+		var album = data.topalbums.album[i].name.replace(/[!'()*]/g, escape);
+		if(data.topalbums.album[i].image[3]['#text']){
+			image = data.topalbums.album[i].image[3]['#text']
+		} else {
+			image = 'https://s-media-cache-ak0.pinimg.com/originals/b8/9d/17/b89d17a8d96248e8ce344de075372c24.jpg';
+		}
+		expandState += "<div id='hiddenCards' class='ui card'><a class='image' id='imageContainer' href='/album/" + encodeURIComponent(artist) + '+' + encodeURIComponent(album) + "'><img id='image' class='img' src='" + image + "'></a><div id='cardInfo' class='ui center aligned container'><h4 id='cardArtist'>" + decodeURI(artist) + "</h4><h7>" + decodeURI(album) + "</h7></div></div>"
+	}
+	currentState = searchAlbumList.innerHTML;
 }
+
+var expandState = '';
+var currentState;
+expand.addEventListener('click', (e) => {
+	if(expand.textContent === 'Expand+'){
+		expand.textContent = 'Expand-';
+		searchAlbumList.innerHTML += expandState;
+	} else {
+		expand.textContent = 'Expand+';
+		searchAlbumList.innerHTML = currentState;
+	}
+})
