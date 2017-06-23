798c4f03b22511de621ab17d0d868e1a14075e3c
smallbrownbike <snscrn@gmail.com>
2017-06-22 18:05:51 -0700

    added random artist suggestion popup

--- public/javascript/albums.js
+++ public/javascript/albums.js
@@ -13,6 +13,12 @@ imageContainer = document.querySelectorAll('#imageContainer'),
 sortAndDelete = document.getElementById('sortAndDelete'),
 trash = document.querySelectorAll('#trash'),
 artistSort = document.getElementById('artistSort'),
+randomImage = document.getElementById('randomImage'),
+message = document.getElementById('message'),
+randomArtistName = document.getElementById('randomArtist'),
+listened = document.getElementById('listened'),
+randomButtonContainer = document.getElementById('randomButtonContainer'),
+similarCollection = document.getElementById('similarCollection'),
 segmentWrapper = document.getElementById('segmentWrapper');
 
 var albums = []
@@ -154,6 +160,22 @@ search.addEventListener('keyup', (e) => {
 	}
 })
 
+if(artists){
+var randomNumber = Math.floor(Math.random() * (artists.length - 1)) + 1
+var randomArtist = artists[randomNumber];
+
+
+function similarListener(){
+	GenerateRandom(JSON.parse(this.responseText))
+}
+var params = {
+	similarRandom: encodeURIComponent(randomArtist)
+}
+var xhr = new XMLHttpRequest();
+xhr.onload = similarListener;
+xhr.open('POST', '/api');
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.send(JSON.stringify(params));
 
 
 
@@ -172,6 +194,87 @@ edit.addEventListener('click', (e) => {
 	}
 })
 
+function GenerateRandom(data){
+	
+	var similar = data.similarartists.artist;
+	(function checkRandom() {
+		var randomSimilarNumber = Math.floor(Math.random() * (similar.length - 1)) + 1;
+		var similarArtist = similar[randomSimilarNumber];
+		if(artists.indexOf(similarArtist.name) < 0){
+			console.log(artists.indexOf(similarArtist))
+			randomImage.innerHTML = '<img src="' + similarArtist.image[3]['#text'] + '">';
+			listened.textContent = similarArtist.name;
+			randomArtistName.textContent = similarArtist.name;
+			randomArtistName.setAttribute('href', '/artist/search/' + encodeURIComponent(similarArtist.name.replace(/[!'()*]/g, escape)));
+			getTopTracks(similarArtist.name)
+		} else {
+			console.log(similarArtist.name)
+			checkRandom()
+		}
+	}) ();
+
+	function getTopTracks(similarArtist){
+		function topTracksListener(){
+		youtube(JSON.parse(this.responseText))
+		}
+		var params = {
+			topTracks: similarArtist
+		}
+		var xhr = new XMLHttpRequest();
+		xhr.onload = topTracksListener;
+		xhr.open('POST', '/api');
+		xhr.setRequestHeader('Content-Type', 'application/json');
+		xhr.send(JSON.stringify(params));
+	}
+
+
+	function youtube(data){
+		arr = data.toptracks.track;
+		arr.forEach((i) => {
+			function youtubeListener(){
+				generateButton(JSON.parse(this.responseText))
+			}
+			var params = {
+				youtube: i.name
+			}
+			var xhr = new XMLHttpRequest();
+			xhr.onload = youtubeListener;
+			xhr.open('POST', '/api');
+			xhr.setRequestHeader('Content-Type', 'application/json');
+			xhr.send(JSON.stringify(params));
+		})
+		id = []
+		function generateButton(data){
+			id.push(data.items[0].id.videoId)
+			if(id.length === 10){
+				var cleanId = []
+				for(var i=0; i<id.length;i++){
+					if(id[i] !== undefined){
+						cleanId.push(id[i])
+					}
+				}
+				listened.setAttribute('href', "https://www.youtube.com/watch_videos?video_ids=" + cleanId.join(','))
+				message.style.display = 'block';
+				similarCollection.style.display = 'block';
+				randomButtonContainer.innerHTML = "<a id='randomButton' class='ui small basic blue button' target='_blank' href='https://www.youtube.com/watch_videos?video_ids=" + cleanId.join(',') + "'>Listen</a>"
+				} 
+				
+		}
+	}
+}
+
+	
+	
+	
+	
+
+/////
+	
+	
+	
+	
+}
+
 var sortedByArtist = '';
 for(var i=0;i<artists.length;i++){
 			var sorted = sortArtists.sort()
@@ -189,13 +292,27 @@ artistSort.addEventListener('click', (e) => {
 	}
 });
 
-/*///link generator
-for(var i=0; i < links.length; i++){
-	var arr = links[i].getAttribute('href').slice(8).split('+');
-	var artist = arr[0].replace(/[!'()*]/g, escape);
-	var album = arr[1].replace(/[!'()*]/g, escape);
-	var encodedLink = '/album/' + encodeURIComponent(artist) + '+' + encodeURIComponent(album);
-	links[i].setAttribute('href', encodedLink)
-}*/
+var closeIcon = document.querySelectorAll('#closeIcon')
+closeIcon.forEach((i) => {
+	i.addEventListener('click', (e) => {
+		similarCollection.style.display = 'none';
+		message.style.display ='none';
+	})
+})
+// closeIcon.addEventListener('click', (e) => {
+// 	similarCollection.style.display = 'none';
+// 	message.style.display ='none';
+// })
 
 };
+
+// /*///link generator
+// for(var i=0; i < links.length; i++){
+// 	var arr = links[i].getAttribute('href').slice(8).split('+');
+// 	var artist = arr[0].replace(/[!'()*]/g, escape);
+// 	var album = arr[1].replace(/[!'()*]/g, escape);
+// 	var encodedLink = '/album/' + encodeURIComponent(artist) + '+' + encodeURIComponent(album);
+// 	links[i].setAttribute('href', encodedLink)
+// }*/
+
+// };
